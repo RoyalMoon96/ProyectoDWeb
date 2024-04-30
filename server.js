8d503d2f0886cefc3c41d2ff77bca6cf1112bb24
RoyalMoon96 <diegoo_doo@hotmail.com>
2024-04-30 17:22:52 -0600

    links

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = require('./app/controllers/router');
+
+const app = express()
+const port = 3000;
+
+app.use(express.static('app'));
+app.use('/views',express.static('views'));
+
+
+app.use(express.json());
+app.use('',router);
+app.listen(port, ()=>{
+    console.log("Aplicación de ejemplo corriendo en puerto ",port);
+});

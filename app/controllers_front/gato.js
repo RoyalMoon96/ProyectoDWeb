082286fc9ecc1d443687bf04f5fddef3e7fec9e1
RoyalMoon96 <diegoo_doo@hotmail.com>
2024-05-02 17:17:02 -0600

    Cambios (CerrarSesion en todas las ventanas)

---
+++ app/controllers_front/gato.js
@@ -0,0 +1,33 @@
+PlayersCount()
+function PlayersCount(){
+    if (window.sessionStorage.length != 2) window.location.href=("http://localhost:3000/home");
+    if (window.sessionStorage.length == 1) {
+        let player1= JSON.parse(sessionStorage.getItem("player1"));
+        if (player1==null) {
+                player1= JSON.parse(sessionStorage.getItem("player2"));
+                sessionStorage.setItem("player1", JSON.stringify(player1));
+                sessionStorage.removeItem("player2");
+                document.getElementById("navbar_player2").innerHTML='<a href="#" id="navbar_player2" style="margin-left: 1cap;"><br><i class="fas fa-user-circle" role="button" style="font-size:36px; color: #FFF4CD; margin-left: 10px;" data-bs-toggle="modal" data-bs-target="#loginModal">Player2</i><br></a>'
+            }
+        document.getElementById("navbar_player1").innerHTML='<a id="navbar_player1" style="margin-left: 1cap;"><br><i role="button" onclick=changeModal("player1") class="fas" style="font-size:36px; color: #FFF4CD; margin-left: 10px;" data-bs-toggle="modal" data-bs-target="#CerrarSesion" ><img style="height:2ch;width:2ch;border-radius:100%;" src="'+player1.img+'">'+player1.nombre+'</i><br></a>'
+        document.getElementById("navbar_player2").innerHTML='<a href="#" id="navbar_player2" style="margin-left: 1cap;"><br><i class="fas fa-user-circle" role="button" style="font-size:36px; color: #FFF4CD; margin-left: 10px;" data-bs-toggle="modal" data-bs-target="#loginModal">Player2</i><br></a>'
+
+    }else if (window.sessionStorage.length == 0){
+        document.getElementById("navbar_player1").innerHTML='<a href="#" id="navbar_player1" style="margin-left: 1cap;"><br><i class="fas fa-user-circle" role="button" style="font-size:36px; color: #FFF4CD; margin-left: 10px;" data-bs-toggle="modal" data-bs-target="#loginModal">Player1</i><br></a>'
+        document.getElementById("navbar_player2").innerHTML='<a href="#" id="navbar_player2" style="margin-left: 1cap;"><br><i class="fas fa-user-circle" role="button" style="font-size:36px; color: #FFF4CD; margin-left: 10px;" data-bs-toggle="modal" data-bs-target="#loginModal">Player2</i><br></a>'
+    }
+    if (window.sessionStorage.length == 2) {
+        let player1= JSON.parse(sessionStorage.getItem("player1"));
+        let player2= JSON.parse(sessionStorage.getItem("player2"));
+        document.getElementById("navbar_player1").innerHTML='<a id="navbar_player1" style="margin-left: 1cap;"><br><i role="button" onclick=changeModal("player1") class="fas" style="font-size:36px; color: #FFF4CD; margin-left: 10px;" data-bs-toggle="modal" data-bs-target="#CerrarSesion"><img style="height:2ch;width:2ch;border-radius:100%;" src="'+player1.img+'">'+player1.nombre+'</i><br></a>'
+        document.getElementById("navbar_player2").innerHTML='<a id="navbar_player2" style="margin-left: 1cap;"><br><i role="button" onclick=changeModal("player2") class="fas" style="font-size:36px; color: #FFF4CD; margin-left: 10px;" data-bs-toggle="modal" data-bs-target="#CerrarSesion"><img style="height:2ch;width:2ch;border-radius:100%;" src="'+player2.img+'">'+player2.nombre+'</i><br></a>'
+    }
+}
+function changeModal(player){
+    document.getElementById("modal_CerrarSesion_userImage").src= JSON.parse(sessionStorage.getItem(player)).img
+    document.getElementById("modal_CerrarSesion_userName").value= JSON.parse(sessionStorage.getItem(player)).nombre
+    document.getElementById("modal_CerrarSesion_btn_Cerrar_Sesion").onclick= function (){
+            sessionStorage.removeItem(player);
+            PlayersCount()
+        }
+}
